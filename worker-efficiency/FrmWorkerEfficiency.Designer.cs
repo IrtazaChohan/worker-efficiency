6d8c4d0ef0f0201deae6e774b383ab9fff103efa
Irtaza Chohan <irtazachohan@gmail.com>
2021-09-30 12:19:12 +0100

    Add project files.

---
+++ worker-efficiency/FrmWorkerEfficiency.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace worker_efficiency
+{
+    partial class FrmWorkerEfficiency
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtWorkerEfficiency = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtWorkerEfficiency
+            // 
+            this.txtWorkerEfficiency.Location = new System.Drawing.Point(143, 60);
+            this.txtWorkerEfficiency.Name = "txtWorkerEfficiency";
+            this.txtWorkerEfficiency.Size = new System.Drawing.Size(103, 23);
+            this.txtWorkerEfficiency.TabIndex = 0;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(145, 103);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(101, 33);
+            this.btnCalculate.TabIndex = 1;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(42, 155);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 15);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Visible = false;
+            this.lblResult.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Worker Efficiency";
+            // 
+            // FrmWorkerEfficiency
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 245);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtWorkerEfficiency);
+            this.Name = "FrmWorkerEfficiency";
+            this.Text = "Worker Efficiency";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtWorkerEfficiency;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ worker-efficiency/FrmWorkerEfficiency.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace worker_efficiency
+{
+    public partial class FrmWorkerEfficiency : Form
+    {
+        public FrmWorkerEfficiency()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        int hours = 0;
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            hours = int.Parse(txtWorkerEfficiency.Text);
+
+            if(hours >= 2 && hours < 3)
+            {
+                lblResult.Text = "You are highly efficient";
+                lblResult.Visible = true;
+            }
+            if (hours >= 3 && hours < 4)
+            {
+                lblResult.Text = "Increase your speed";
+                lblResult.Visible = true;
+            }
+            if (hours >= 4 && hours < 5)
+            {
+                lblResult.Text = "Your rubbish and need extra training";
+                lblResult.Visible = true;
+            }
+            if (hours >= 5)
+            {
+                lblResult.Text = "YOUR SACKED - get out of the company...SECURITY!";
+                lblResult.Visible = true;
+            }
+        }
+    }
+}
